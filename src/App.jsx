00fbb31d6082c9c1ea316a07d621759b448feb11
The GreatPratyush <pratushgupta27199@gmail.com>
2026-04-06 16:57:18 +0530

    cssAdded

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css"
 const App = ()=>{
   let [computerScore,setInitailState] = useState(0)
   let [yourScore,setInitailState2] = useState(0)
